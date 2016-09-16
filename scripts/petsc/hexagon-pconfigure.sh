9322515aa4eec553d06bca1952526d3074b152e5
Abdoulaye SAMAKE <abdoulaye.samake@nersc.no>
2016-09-16 15:37:26 +0000

    Add scripts (external packages installation)

---
+++ scripts/petsc/hexagon-pconfigure.sh
@@ -0,0 +1,35 @@
+export PETSC_PREFIX=$WORK/packages/petsc/3.6.3
+
+./configure \
+    -with-python \
+    --with-debugging=0 \
+    --with-c-support=1 \
+    --with-c++-support=1 \
+    --with-shared-libraries=1 \
+    --with-mpi-dir=/opt/cray/mpt/7.2.5/gni/mpich2-gnu/51 \
+    --with-blas-lapack-lib="-L/opt/cray/libsci/13.2.0/GNU/4.9/x86_64/lib -lsci_gnu_mp" \
+    --CFLAGS='-O3' \
+    --CXXFLAGS='-O3' \
+    --FFLAGS='-O3' \
+    --with-blacs \
+    --download-blacs=yes \
+    --with-scalapack=1 \
+    --download-scalapack=yes \
+    --with-mumps=1 \
+    --download-mumps=yes \
+    --with-superlu=1 \
+    --download-superlu=yes \
+    --with-superlu_dist=1 \
+    --download-superlu_dist=yes \
+    --with-ml=1 \
+    --download-ml=yes \
+    --with-suitesparse=1 \
+    --download-suitesparse=yes \
+    --with-metis=1 \
+    --download-metis=yes \
+    --with-parmetis=1 \
+    --download-parmetis=yes \
+    --with-ptscotch=1 \
+    --download-ptscotch=yes \
+    --with-c2html=0 \
+    --prefix=$PETSC_PREFIX
