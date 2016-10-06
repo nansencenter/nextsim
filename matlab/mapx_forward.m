97668018da7a4507e5f32768229bd67b0852607d
Einar Ólason <einar.olason@nersc.no>
2016-10-06 12:11:06 +0200

    Mex files so that we can use the mapx projections we use in the model in Matlab. This will NOT give the same results as m_ll2xy and m_xy2ll.

---
+++ matlab/mapx_forward.m
@@ -0,0 +1,10 @@
+function [x, y] = mapx_forward(filename, lon, lat)
+
+% A convert a set of lon and lat (in degrees) to x and y coordinates using the mapx libraries.
+%
+% Usage:
+% >> [x, y] = mapx_forward(filename, lon, lat)
+%
+% Where filename is the name of the mpp file (e.g. 'NpsNextsim.mpp' or '../data/NpsNextsim.mpp')
+
+error('mex-file not found')
