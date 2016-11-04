a7854ccd048fa056543ea36a759fd7c77697526a
Abdoulaye SAMAKE <abdoulaye.samake@nersc.no>
2016-11-04 14:37:42 +0100

    update bash scrpits (for running the model)

--- model/run.sh
+++ model/run.sh
@@ -7,8 +7,9 @@ bin/nextsim.exec --config-files=nextsim.cfg
 if [ \( "$NEXTSIM_BUILD_TYPE" == "Debug" \) -o \( "$NEXTSIM_BUILD_TYPE" == "debug" \) ]; then
 	echo "============================================"
 	echo "CPU profiling starts..."
-	echo "pprof --pdf --functions --focus=run --cum --drop_negative --nodecount=50 bin/nextsim.exec profile.log > profile.pdf"
-	pprof --pdf --functions --focus=run --cum --drop_negative --nodecount=50 bin/nextsim.exec profile.log > profile.pdf
+	CMDSTRPROF="pprof --pdf --functions --focus=run --cum --drop_negative --nodecount=50 bin/nextsim.exec profile.log > profile.pdf"
+	echo $CMDSTRPROF
+	eval $CMDSTRPROF
 	echo "CPU profiling done"
 	echo "============================================"
 	echo "Run the following command to analyze the CPU profile:"
