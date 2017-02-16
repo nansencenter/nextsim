3f040da73dc7aa8069196b16e2958467cc727c08
Sylvain Bouillon <sylvain.bouillon@nersc.no>
2017-02-16 17:40:11 +0100

    new function to read bin files, useful for data assimilation

--- matlab/neXtSIM_bin_revert.m
+++ matlab/neXtSIM_bin_revert.m
@@ -12,16 +12,20 @@ function [mesh_out,data_out] = neXtSIM_bin_revert(dirname, proc, step)%,varargin
 % if nVarargs >= 1, save_flag = varargin{1}; end
 % if nVarargs >= 2, error('Too many inputs'), end
 
+if(~isempty(dirname)&& dirname(end)~='/')
+    dirname=[dirname, '/'];
+end
+
 if(isempty(proc))
-    field_info  = [ dirname '/field_' num2str(step) '.dat'];
-    field_data  = [ dirname '/field_' num2str(step) '.bin'];
-    mesh_info   = [ dirname '/mesh_' num2str(step) '.dat'];
-    mesh_data   = [ dirname '/mesh_' num2str(step) '.bin'];
+    field_info  = [ dirname 'field_' num2str(step) '.dat'];
+    field_data  = [ dirname 'field_' num2str(step) '.bin'];
+    mesh_info   = [ dirname 'mesh_' num2str(step) '.dat'];
+    mesh_data   = [ dirname 'mesh_' num2str(step) '.bin'];
 else
-    field_info  = [ dirname '/field_' num2str(proc) '_' num2str(step) '.dat'];
-    field_data  = [ dirname '/field_' num2str(proc) '_' num2str(step) '.bin'];
-    mesh_info   = [ dirname '/mesh_' num2str(proc) '_' num2str(step) '.dat'];
-    mesh_data   = [ dirname '/mesh_' num2str(proc) '_' num2str(step) '.bin'];
+    field_info  = [ dirname 'field_' num2str(proc) '_' num2str(step) '.dat'];
+    field_data  = [ dirname 'field_' num2str(proc) '_' num2str(step) '.bin'];
+    mesh_info   = [ dirname 'mesh_' num2str(proc) '_' num2str(step) '.dat'];
+    mesh_data   = [ dirname 'mesh_' num2str(proc) '_' num2str(step) '.bin'];
 end
 
 
@@ -37,39 +41,3 @@ end
 
 end
 
-function out=read_bin_export(file_info,file_data)
-%Getting info
-disp(['Read: ' file_info])
-fileID = fopen(file_info,'r');
-info_tmp=textscan(fileID,'%s %s');
-data_names=info_tmp{1};
-data_types=info_tmp{2};
-fclose(fileID);
-
-field = cell(numel(data_names));
-fileID = fopen(file_data,'r');
-for i = 1:numel(data_names)
-    field{i} = data_names{i};
-    %if data_types(i)==8
-    %   prec = 'double';
-    % else
-    %   prec = 'int';
-    % end
-
-    if strcmp(data_types(i),'double') || strcmp(data_types(i),'8') % check against '8' for backwards compatiblity
-	  prec = 'double';
-	elseif strcmp(data_types(i),'float')
-	  prec = 'single';
-	else
-	  prec = 'int';
-    end
-
-    N_data=fread(fileID,1,'int');
-    if strcmp('Time',field{i})
-        out.(field{i}) = fread(fileID,N_data,prec) + datenum(1900,1,1);
-    else
-        out.(field{i}) = fread(fileID,N_data,prec);
-    end
-end
-fclose(fileID);
-end
