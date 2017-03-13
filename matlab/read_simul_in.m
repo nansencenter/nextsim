4c7ee41fbbf9cae7d00da8c7c8811944558272ff
Pierre Rampal <pierre.rampal@nersc.no>
2017-03-13 12:00:21 +0100

    BE AWARE: Change the Makefile to the 2016b matlab version.

--- matlab/read_simul_in.m
+++ matlab/read_simul_in.m
@@ -6,6 +6,7 @@ function simul_in=read_simul_in(saved_simul_in)
 simul_in.time_init=datenum(get_value(saved_simul_in,'simul.time_init'),'yyyy-mm-ddHH:MM:SS');
 simul_in.duration=str2num(get_value(saved_simul_in,'simul.duration'));
 simul_in.output_per_day=str2num(get_value(saved_simul_in,'simul.output_per_day'));
+simul_in.mesh_filename=get_value(saved_simul_in,'simul.mesh_filename');
 
 end
 
