e65b6cc832f6724c85e40ae4b609bc6ac0cc4faf
Timothy Williams <tdcwilliams@gmail.com>
2017-03-17 16:14:35 +0100

    minor changes

--- model/finiteelement.cpp
+++ model/finiteelement.cpp
@@ -378,7 +378,7 @@ void
 FiniteElement::checkReloadDatasets(external_data_vec const& ext_data_vec,
         double const& CRtime, std::string const& printout)
 {
-    std::cout<<"size of external data vector"<<ext_data_vec.size()<<"\n";
+    std::cout<<"size of external data vector = "<<ext_data_vec.size()<<"\n";
     if ( ext_data_vec.size()==0 )
     {
         LOG(DEBUG) <<"check_and_reload ("<<printout<<"):\n";
@@ -7390,7 +7390,7 @@ FiniteElement::wimToNextsim(bool step)
     }
 #endif
 
-    std::cout<<"Finished wimToNextsim";
+    std::cout<<"Finished wimToNextsim\n";
 }//wimToNextsim
 #endif
 
