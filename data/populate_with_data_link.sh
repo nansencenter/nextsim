61de23e010d649f0aa1f3b36087123ea25c5bf61
Sylvain Bouillon <sylvain@seoul.ad.nersc.no>
2017-03-31 17:25:32 +0200

    more generic version

--- data/populate_with_data_link.sh
+++ data/populate_with_data_link.sh
@@ -1,10 +1,14 @@
 #! /bin/sh
 
-# link the data
-ln -s /Data/sim/data/BATHYMETRY/*.nc .
-ln -s /Data/sim/data/TOPAZ4/198910_201312/*.nc .
-ln -s /Data/sim/data/ERAI/*.nc .
-ln -s /Data/sim/data/ASR_FINAL/*.nc .
-ln -s /Data/sim/data/AMSR2_ice_conc/*.nc .
-ln -s /Data/sim/data/AMSRE_ice_conc/*/*.nc .
+export NEXTSIM_DATADIR=/net/sverdrup-1/vol/sim/data
 
+# link the data
+ln -s $NEXTSIM_DATADIR/BATHYMETRY/*.nc .
+ln -s $NEXTSIM_DATADIR/TOPAZ4/198910_201512/*.nc .
+ln -s $NEXTSIM_DATADIR/ERAI/*.nc .
+ln -s $NEXTSIM_DATADIR/ASR_FINAL/*.nc .
+ln -s $NEXTSIM_DATADIR/AMSR2_ice_conc/*.nc .
+for year in {2002..20011..1}
+	do
+		ln -s $NEXTSIM_DATADIR/AMSRE_ice_conc/year/*.nc .
+	done
