64bd1fde014b29560c37bd6f33e30a1c431f0c98
Sylvain Bouillon <syloui@johansen.ad.nersc.no>
2017-04-22 18:21:41 +0200

    add datasets

--- data/populate_with_data_link.sh
+++ data/populate_with_data_link.sh
@@ -7,4 +7,5 @@ ln -s /Data/sim/data/ERAI/*.nc .
 ln -s /Data/sim/data/ASR_FINAL/*.nc .
 ln -s /Data/sim/data/AMSR2_ice_conc/*.nc .
 ln -s /Data/sim/data/AMSRE_ice_conc/*/*.nc .
-
+ln -s /Data/sim/data/OSISAF_ice_type/*/*/*.nc .
+ln -s /Data/sim/data/SIT_data/icesat_filled_10prods/*.nc .
