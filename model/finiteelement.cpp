2f2f8999d505e0537b9ed094af2207665d7aad25
Sylvain Bouillon <syloui@johansen.ad.nersc.no>
2017-06-10 16:16:19 +0200

    Remove this useless comment

--- model/finiteelement.cpp
+++ model/finiteelement.cpp
@@ -424,7 +424,7 @@ void
 FiniteElement::checkReloadDatasets(external_data_vec const& ext_data_vec,
         double const& CRtime, std::string const& printout)
 {
-    std::cout<<"size of external data vector = "<<ext_data_vec.size()<<"\n";
+    //std::cout<<"size of external data vector = "<<ext_data_vec.size()<<"\n";
     if ( ext_data_vec.size()==0 )
     {
         LOG(DEBUG) <<"check_and_reload ("<<printout<<"):\n";
