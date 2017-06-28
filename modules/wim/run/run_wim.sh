4c3d00040439f25102ebe2863d9884160b56bede
tdcwilliams <tdcwilliams@gmail.com>
2017-06-28 11:12:05 +0200

    changes to wim stand-alone script

--- modules/wim/run/run_wim.sh
+++ modules/wim/run/run_wim.sh
@@ -1,1 +1,2 @@
-bin/WIM2d_single_call_cpp.exec --config-files=wim.cfg
+# export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$NEXTSIMDIR/lib # not needed on mac
+bin/WIM2d_single_call_cpp.exec --config-file=wim.cfg
