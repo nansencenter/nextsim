98186ecdc8735a641422a3dea1fc3f63e182796f
tdcwilliams <tdcwilliams@gmail.com>
2017-06-28 11:01:12 +0200

    line in run script not needed for stand-alone wim script

--- modules/wim/run/run_wim.sh
+++ modules/wim/run/run_wim.sh
@@ -1,2 +1,2 @@
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$NEXTSIMDIR/lib
+# export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$NEXTSIMDIR/lib # not needed on mac
 bin/WIM2d_single_call_cpp.exec --config-file=wim.cfg
