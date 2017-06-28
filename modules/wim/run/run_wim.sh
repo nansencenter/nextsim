ae0ba7b82d0b281936d193b2536444e44daf8ccd
tdcwilliams <tdcwilliams@gmail.com>
2017-06-28 12:37:17 +0200

    minor changes to wim standalone stuff

--- modules/wim/run/run_wim.sh
+++ modules/wim/run/run_wim.sh
@@ -1,2 +1,7 @@
 # export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$NEXTSIMDIR/lib # not needed on mac
 bin/WIM2d_single_call_cpp.exec --config-file=wim.cfg
+
+echo " "
+echo "To plot results, run"
+echo "\$WIM2D_PATH/fortran/tools/plot_prog.sh"
+$WIM2D_PATH/fortran/tools/plot_prog.sh
