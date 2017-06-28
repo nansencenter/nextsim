e90c029e5c1a14707051eaa7cea98c0a9e530916
tdcwilliams <tdcwilliams@gmail.com>
2017-06-28 15:20:50 +0200

    standalone wim runs on johansen now

--- modules/wim/run/run_wim.sh
+++ modules/wim/run/run_wim.sh
@@ -1,4 +1,8 @@
-# export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$NEXTSIMDIR/lib # not needed on mac
+kernel=`uname -s`
+if [ "$kernel" == "Linux" ]
+then
+   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NEXTSIMDIR/lib:$BOOST_DIR/lib
+fi
 bin/WIM2d_single_call_cpp.exec --config-file=wim.cfg
 
 echo " "
