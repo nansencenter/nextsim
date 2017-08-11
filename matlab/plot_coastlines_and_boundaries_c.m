f229f1dbaeca76f74162e6c0788f87c9637e7428
tdcwilliams <tdcwilliams@gmail.com>
2017-08-11 15:17:03 +0200

    plot using x,y for testing; hold on

--- matlab/plot_coastlines_and_boundaries_c.m
+++ matlab/plot_coastlines_and_boundaries_c.m
@@ -32,8 +32,6 @@ else
 end
 
 boundary   = mesh.boundary.from_msh;
-node_lat   = mesh.node.lat;
-node_lon   = mesh.node.lon;
 
 %Selecting closed boundaries
 fix = find(flag_boundary_fix==boundary(:,3));
@@ -45,30 +43,47 @@ for loop_i=1:length(flag_boundary_free)
     free= [free;find(fbf==boundary(:,3))];
 end
 
-closed_boundaryLat  = node_lat(boundary(fix ,1:2,1))';
-closed_boundaryLon  = node_lon(boundary(fix ,1:2,1))';
-free_boundaryLat = node_lat(boundary(free ,1:2,1))';
-free_boundaryLon = node_lon(boundary(free ,1:2,1))';
+sfac  = 1e-3;%km
+if 1
+   %use lon/lat
+   node_lat   = mesh.node.lat;
+   node_lon   = mesh.node.lon;
+   closed_boundaryLat  = node_lat(boundary(fix ,1:2,1))';
+   closed_boundaryLon  = node_lon(boundary(fix ,1:2,1))';
+   free_boundaryLat = node_lat(boundary(free ,1:2,1))';
+   free_boundaryLon = node_lon(boundary(free ,1:2,1))';
 
-mppfile = which('NpsNextsim.mpp');
-if strcmp(mppfile,'')
-   error('add path to mppfile');
+   mppfile = which('NpsNextsim.mpp');
+   if strcmp(mppfile,'')
+      error('add path to mppfile');
+   end
+
+   % projection
+   [closed_boundaryX,closed_boundaryY]= mapx_forward(mppfile,closed_boundaryLon(:)',closed_boundaryLat(:)');
+   [free_boundaryX,free_boundaryY]= mapx_forward(mppfile,free_boundaryLon(:)',free_boundaryLat(:)');
+
+   %reshape and scaling to km
+   closed_boundaryX  = reshape(sfac*closed_boundaryX,size(closed_boundaryLon));
+   closed_boundaryY  = reshape(sfac*closed_boundaryY,size(closed_boundaryLon));
+   free_boundaryX    = reshape(sfac*free_boundaryX,size(free_boundaryLon));
+   free_boundaryY    = reshape(sfac*free_boundaryY,size(free_boundaryLon));
+else
+   %use x/y
+   node_x   = sfac*mesh.node.x;
+   node_y   = sfac*mesh.node.y;
+   closed_boundaryX  = node_x(boundary(fix ,1:2,1))';
+   closed_boundaryY  = node_y(boundary(fix ,1:2,1))';
+   free_boundaryX    = node_x(boundary(free ,1:2,1))';
+   free_boundaryY    = node_y(boundary(free ,1:2,1))';
 end
 
-% projection
-[closed_boundaryX,closed_boundaryY]= mapx_forward(mppfile,closed_boundaryLon(:)',closed_boundaryLat(:)');
-[free_boundaryX,free_boundaryY]= mapx_forward(mppfile,free_boundaryLon(:)',free_boundaryLat(:)');
-
-%reshape and scaling to km
-closed_boundaryX=reshape(0.001*closed_boundaryX,size(closed_boundaryLon));
-closed_boundaryY=reshape(0.001*closed_boundaryY,size(closed_boundaryLon));
-free_boundaryX=reshape(0.001*free_boundaryX,size(free_boundaryLon));
-free_boundaryY=reshape(0.001*free_boundaryY,size(free_boundaryLon));
 
 %Plotting closed mesh boundaries (e.g. coastlines)
 plot(closed_boundaryX,closed_boundaryY,'Color',[0.3 0.3 0.3],'LineWidth',0.2);
+hold on;
 %plot(closed_boundaryX,closed_boundaryY,'Color',[1 1 1],'LineWidth',0.2);
 %Plotting open mesh boundaries
 plot(free_boundaryX,free_boundaryY,'g','LineWidth',2);
+hold off;
 
 return;
