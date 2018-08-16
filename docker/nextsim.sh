1780f5bec3afd098f6977fcf78cd36a63190cd75
akorosov <korosov@gmail.com>
2018-08-16 14:10:28 +0200

    #51 fix test.cfg and nextsim.sh

--- docker/nextsim.sh
+++ docker/nextsim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-export SIMDATADIR=/nextsim/nextsim
-export SIMFORECASTDIR=/nextsim/forecast
-/nextsim/model/bin/nextsim.exec --config-files=/nextsim/model/bin/test.cfg
+export NEXTSIMDIR=/nextsimdir
+export SIMDATADIR=/simdatadir
+export SIMFORECASTDIR=/simforecastdir
+mpirun --allow-run-as-root -np 4 /nextsim/model/bin/nextsim.exec -mat_mumps_icntl_14 60 --config-files=/nextsimdir/docker/test.cfg
