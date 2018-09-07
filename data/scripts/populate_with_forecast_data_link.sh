fd64d45358b945bbedf5ee6921c52d55aa96c021
tdcwilliams <tdcwilliams@gmail.com>
2018-09-07 14:37:56 +0200

    update data/scripts/populate_with_forecast_data_link.sh * new data dir structure * command line argument taken

--- data/scripts/populate_with_forecast_data_link.sh
+++ data/scripts/populate_with_forecast_data_link.sh
@@ -1,29 +1,35 @@
 #! /bin/bash
+#
+# Link the ocean forecast files from ROOT_DATA_DIR (1st command line input to script)
+# which has all the needed data (eg /Data/nextsimf/data on johansen)"
+# 
+# Usage: `basename $thisfile` [ROOT_DATA_DIR]"
+# Where ROOT_DATA_DIR is the directory where the data will be linked from"
 
-if [ -z "$LOCALFORECASTDATADIR" ]; then 
-	echo "LOCALFORECASTDATADIR is unset, we try to link withe the data from /Data/nextsim/data"; 
+thisfile=$0
+thisdir=`dirname $thisfile`
+CWD=`pwd`
 
-	TMP_NEXTSIM_FORECAST_DATADIR=/Data/nextsimf/data
-	if [ -d "$TMP_NEXTSIM_FORECAST_DATADIR" ]; then
-		echo "Found a directory $TMP_NEXTSIM_FORECAST_DATADIR"
-  		# Control will enter here if $DIRECTORY exists.
-		export NEXTSIM_FORECAST_DATADIR=$TMP_NEXTSIM_FORECAST_DATADIR
-	fi
-
-else 
-	echo "data are linked from '$LOCALFORECASTDATADIR'"; 
-	
-	TMP_NEXTSIM_DATADIR=$LOCALFORECASTDATADIR
-	if [ -d "$TMP_NEXTSIM_FORECAST_DATADIR" ]; then
-  		# Control will enter here if $DIRECTORY exists.
-		export NEXTSIM_FORECAST_DATADIR=$TMP_NEXTSIM_FORECAST_DATADIR
-	fi
+if [ $# -eq 0 ]
+then
+   echo "Usage: `basename $thisfile` [ROOT_DATA_DIR]"
+   echo "Where ROOT_DATA_DIR is the directory where the data will be linked from"
+   echo "(eg /Data/nextsimf/data on johansen)"
+   exit 1
 fi
 
-
-if [ -z "$NEXTSIM_FORECAST_DATADIR" ]; then 
-	echo "Not able to define LOCALFORECASTDATADIR, some data may not be linked"; 
-else
-	# link the data
-	ln -s $NEXTSIM_FORECAST_DATADIR/TOPAZ4RC_daily/*.nc .
+ROOT_DATA_DIR="$1"
+if [ ! -d "$ROOT_DATA_DIR" ]
+then
+   echo "Input directory << $ROOT_DATA_DIR >> does not exist"
+   exit 1
 fi
+
+# link the data
+NETCDF_LINKS_DIR=$CWD/netcdf_data_links
+mkdir -p $NETCDF_LINKS_DIR
+cd $NETCDF_LINKS_DIR
+ln -s $ROOT_DATA_DIR/TOPAZ4RC_daily/*.nc .
+
+# lot of links so update the list of files
+ls -lh > FileList.txt
