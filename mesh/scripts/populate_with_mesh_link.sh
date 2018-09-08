7ddad2e361948e8d20ee29388b1e2c3a3ba7b669
tdcwilliams <tdcwilliams@gmail.com>
2018-09-08 23:13:39 +0200

    * modify populate_with_mesh_links.sh script to handle restructured mesh dir #113 * add README

--- mesh/scripts/populate_with_mesh_link.sh
+++ mesh/scripts/populate_with_mesh_link.sh
@@ -1,40 +1,59 @@
 #! /bin/bash
+#
+# Link the files from ROOT_MESH_DIR (1st command line input to script)
+# which has all the needed data (eg /Data/sim/data/mesh on johansen)"
+# 
+# Usage: `basename $thisfile` [ROOT_MESH_DIR]"
+# Where ROOT_MESH_DIR is the directory where the data will be linked from"
 
-if [ -z "$LOCALDATADIR" ]; then 
-	echo "LOCALDATADIR is unset, we try to link with the data from /Data/sim/data or /net/sverdrup-1/vol/sim/data"; 
+thisfile=$0
+thisdir=`dirname $thisfile`
+CWD=`pwd`
 
-	TMP_NEXTSIM_DATADIR=/net/sverdrup-1/vol/sim/data
-	if [ -d "$TMP_NEXTSIM_DATADIR" ]; then
-  		# Control will enter here if $DIRECTORY exists.
-		export NEXTSIM_DATADIR=$TMP_NEXTSIM_DATADIR
-	fi
-
-	TMP_NEXTSIM_DATADIR=/Data/sim/data
-	if [ -d "$TMP_NEXTSIM_DATADIR" ]; then
-  		# Control will enter here if $DIRECTORY exists.
-		export NEXTSIM_DATADIR=$TMP_NEXTSIM_DATADIR
-	fi
-
-else 
-	echo "data are linked from '$LOCALDATADIR'"; 
-	
-	TMP_NEXTSIM_DATADIR=$LOCALDATADIR
-	if [ -d "$TMP_NEXTSIM_DATADIR" ]; then
-  		# Control will enter here if $DIRECTORY exists.
-		export NEXTSIM_DATADIR=$TMP_NEXTSIM_DATADIR
-	fi
+if [ $# -eq 0 ]
+then
+   echo "Usage: `basename $thisfile` [ROOT_MESH_DIR]"
+   echo "Where ROOT_MESH_DIR is the directory where the data will be linked from"
+   echo "(eg /Data/sim/data/mesh on johansen)"
+   exit 1
 fi
 
-if [ -z "$NEXTSIM_DATADIR" ]; then 
-	echo "Not able to define NEXTSIM_DATADIR, some mesh may not be linked"; 
-else
-	# link the meshes
-	echo "We link the meshes from $NEXTSIM_DATADIR"; 
-	ln -s $NEXTSIM_DATADIR/mesh/unref/*.msh .
-	ln -s $NEXTSIM_DATADIR/mesh/split/*.msh .
+ROOT_MESH_DIR="$1"
+if [ ! -d "$ROOT_MESH_DIR" ]
+then
+   echo "Input directory << $ROOT_MESH_DIR >> does not exist"
+   exit 1
+fi
 
-        # link WIM grids
-        mkdir wim_grids
-        cd wim_grids
-	ln -s $NEXTSIM_DATADIR/mesh/wim_grids/*.* .
+MESH_LINKS_DIR=$CWD/meshfile_links
+mkdir -p $MESH_LINKS_DIR
+cd $MESH_LINKS_DIR
+
+# link the meshes
+echo "We link the meshes from $ROOT_MESH_DIR"; 
+ln -s $ROOT_MESH_DIR/unref/*.msh .
+ln -s $ROOT_MESH_DIR/split/*.msh .
+
+# link WIM grids
+mkdir -p wim_grids
+cd wim_grids
+ln -s $ROOT_MESH_DIR/wim_grids/*.* .
+
+# link to .mpp files if they are not present
+NEXTSIM_MESH_SRC=`readlink -f $thisdir/..`
+cd $CWD
+if [ ! -d mpp_files  ]
+then
+   mkdir -p mpp_files
+   cd mpp_files
+   ln -s $NEXTSIM_MESH_SRC/mpp_files/* .
+fi
+
+# link to .geo files if they are not present
+cd $CWD
+if [ ! -d geo_files  ]
+then
+   mkdir -p geo_files
+   cd geo_files
+   ln -s $NEXTSIM_MESH_SRC/geo_files/* .
 fi
