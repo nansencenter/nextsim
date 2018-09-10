b4ee17ffc0b8a1a54ef5be6d70a0de8cdbeac99a
tdcwilliams <tdcwilliams@gmail.com>
2018-09-10 15:11:08 +0200

    rewrite code with comments added #113

--- scripts/run_docker_nextsim_vienna.sh
+++ scripts/run_docker_nextsim_vienna.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+VOLUMES=()
+
+# mount some directories as themselves so that various links to data work (read-only)
+VOLUMES+=("-v /Data/sim/data/:/Data/sim/data/:ro")
+VOLUMES+=("-v /Data/nextsimf/data/:/Data/nextsimf/data/:ro")
+VOLUMES+=("-v /mnt/10.11.12.231/sim/:/mnt/10.11.12.231/sim/:ro")
+VOLUMES+=("-v /mnt/10.11.12.232/nextsimf/:/mnt/10.11.12.232/nextsimf/:ro")
+
+# $NEXTSIM_MESH_DIR (read-only)
+VOLUMES+=("-v /home/timill/Github-Repos/nextsim-cxx/nextsim/mesh:/nextsim_mesh_dir/:ro")
+
+# $NEXTSIM_DATA_DIR (read-only)
+VOLUMES+=("-v /home/timill/Github-Repos/nextsim-cxx/nextsim/data:/nextsim_data_dir/:ro")
+
+# where docker can find inputs and outputs (read-write)
+# NB could separate input and output if you preferred
+VOLUMES+=("-v /home/timill/docker_run/:/docker_io/")
+
+
+ARGS=()
+# arguments for run_in_docker.sh
+# - comment this line to just enter the container without running the model
+ARGS+=(/docker_io/nextsim_model_options_parallel.cfg 2)
+
 docker run --rm -it \
     --security-opt seccomp=unconfined \
-    -v /Data/sim/data/:/Data/sim/data/:ro \
-    -v /Data/nextsimf/data/:/Data/nextsimf/data/:ro \
-    -v /mnt/10.11.12.231/sim/:/mnt/10.11.12.231/sim/:ro \
-    -v /mnt/10.11.12.232/nextsimf/:/mnt/10.11.12.232/nextsimf/:ro \
-    -v /home/timill/Github-Repos/nextsim-cxx/nextsim/mesh:/nextsim_mesh_dir/:ro \
-    -v /home/timill/Github-Repos/nextsim-cxx/nextsim/data:/nextsim_data_dir/:ro \
-    -v /home/timill/docker_run/:/docker_io/ \
-    nextsim /docker_io/nextsim_model_options_parallel.cfg 2
-    #nextsim
+    ${VOLUMES[@]} nextsim ${ARGS[@]}
