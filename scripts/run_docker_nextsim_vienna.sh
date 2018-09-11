edf70ae32000c072d75e2f9cc230efa1d09396f0
tdcwilliams <tdcwilliams@gmail.com>
2018-09-11 13:55:58 +0200

    update scripts/run_docker_nextsim_vienna.sh for changes to data/mesh structures #113

--- scripts/run_docker_nextsim_vienna.sh
+++ scripts/run_docker_nextsim_vienna.sh
@@ -3,16 +3,19 @@
 VOLUMES=()
 
 # mount some directories as themselves so that various links to data work (read-only)
+# NB some data is linked from the nextsim source directory
+# eg .mpp files, .geo files
 VOLUMES+=("-v /Data/sim/data/:/Data/sim/data/:ro")
 VOLUMES+=("-v /Data/nextsimf/data/:/Data/nextsimf/data/:ro")
 VOLUMES+=("-v /mnt/10.11.12.231/sim/:/mnt/10.11.12.231/sim/:ro")
 VOLUMES+=("-v /mnt/10.11.12.232/nextsimf/:/mnt/10.11.12.232/nextsimf/:ro")
+VOLUMES+=("-v /home/timill/Github-Repos/nextsim-cxx/nextsim:/home/timill/Github-Repos/nextsim-cxx/nextsim:ro")
 
 # $NEXTSIM_MESH_DIR (read-only)
-VOLUMES+=("-v /home/timill/Github-Repos/nextsim-cxx/nextsim/mesh:/nextsim_mesh_dir/:ro")
+VOLUMES+=("-v /home/timill/Github-Repos/nextsim-cxx/nextsim/mesh/mesh_links:/nextsim_mesh_dir/:ro")
 
 # $NEXTSIM_DATA_DIR (read-only)
-VOLUMES+=("-v /home/timill/Github-Repos/nextsim-cxx/nextsim/data:/nextsim_data_dir/:ro")
+VOLUMES+=("-v /home/timill/Github-Repos/nextsim-cxx/nextsim/data/data_links:/nextsim_data_dir/:ro")
 
 # where docker can find inputs and outputs (read-write)
 # NB could separate input and output if you preferred
