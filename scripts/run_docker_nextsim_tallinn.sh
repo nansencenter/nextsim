b664fb12b328ebcea304e30c1491ad2fcc1010b5
akorosov <korosov@gmail.com>
2018-09-13 08:44:30 +0200

    #113 change docker dirs to /data and /mesh

--- scripts/run_docker_nextsim_tallinn.sh
+++ scripts/run_docker_nextsim_tallinn.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker run --rm -it \
     --security-opt seccomp=unconfined \
-    -v /home/antonk/nextsim/nextsim:/nextsim \
-    -v /Data/sim/data/:/Data/sim/data/ \
-    -v /Data/nextsimf/data/:/Data/nextsimf/data/ \
-    -v /Data/nextsimf/src/nextsim-cxx/nextsim-develop-new/data/:/simdata/data \
-    -v /home/antonk/nextsim/nextsimf_forecasts/:/simforecast/ \
-    nextsim /nextsim/test.cfg 7
+    -v /home/antonk/nextsim/nextsim/data:/data \
+    -v /home/antonk/nextsim/nextsim/mesh:/mesh \
+    -v /Data/sim/data:/Data/sim/data \
+    -v /Data/nextsimf/data:/Data/nextsimf/data \
+    -v /home/antonk/nextsim/forecasts:/forecasts \
+    nextsim /forecasts/test.cfg 7
