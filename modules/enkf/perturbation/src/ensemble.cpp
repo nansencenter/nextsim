561ad4ce39da45c02b79987b06690b7277962969
Ali Aydogdu <ali.aydogdu@nersc.no>
2018-12-14 13:39:48 +0100

    methods to read ranfld.dat and perturb wind fields

--- modules/enkf/perturbation/src/ensemble.cpp
+++ modules/enkf/perturbation/src/ensemble.cpp
@@ -1,6 +1,8 @@
 #include "ensemble.hpp"
 
-void ensemble::generate_ensemble(int rdm, int cdm)
+namespace Nextsim
+{
+void Nextsim::ensemble::generate_ensemble(int rdm, int cdm)
 	{ 
 		float ranfld[rdm][cdm];
 
@@ -37,6 +39,50 @@ void ensemble::generate_ensemble(int rdm, int cdm)
                     synoptic.precip.data.push_back(ranfld[i][synoptic.precip.id]);
                     synoptic.relhum.data.push_back(ranfld[i][synoptic.relhum.id]);
                 } 
+};
+
+
+void Nextsim::ensemble::generate_ensemble(std::vector<double>& loaded_uwind, std::vector<double>& loaded_vwind, int rdm, int cdm)
+	{ 
+		float ranfld[rdm*cdm][8];
+
+		// Call fortran library for synoptic perturbation
+		// A file called ranfld.dat will be written
+		p_pseudo2D_fld_sub();
+		
+		// Find and read the ranfld.dat into struct synoptic
+		ifstream franfld;
+		franfld.open(ranfile);
+                int row = 0;
+                while(!franfld.eof()){
+                   std::string str;
+                   std::getline(franfld, str);
+                   std::stringstream ss(str);
+                   int col = 0;
+                   while(ss >> ranfld[row][col]) col++;
+                
+                   row++;
+                } 
+		franfld.close();
+
+                synoptic.uwind.data.reserve(rdm);
+                synoptic.vwind.data.reserve(rdm);
+                synoptic.slp.data.reserve(rdm);
+                synoptic.t2air.data.reserve(rdm);
+                synoptic.precip.data.reserve(rdm);
+                synoptic.relhum.data.reserve(rdm);
+                for(int i=0; i<rdm; i++) { 
+                    synoptic.uwind.data.push_back (ranfld[i][synoptic.uwind.id] );
+                    synoptic.vwind.data.push_back (ranfld[i][synoptic.vwind.id] );
+                    synoptic.t2air.data.push_back (ranfld[i][synoptic.t2air.id] );
+                    synoptic.slp.data.push_back   (ranfld[i][synoptic.slp.id]   );
+                    synoptic.precip.data.push_back(ranfld[i][synoptic.precip.id]);
+                    synoptic.relhum.data.push_back(ranfld[i][synoptic.relhum.id]);
+                } 
+                for(int i=0; i<rdm; i++) { 
+		loaded_uwind[i] = loaded_uwind[i] + synoptic.uwind.data[i];
+		loaded_vwind[i] = loaded_vwind[i] + synoptic.vwind.data[i];
+		}
 
 
 		/*
@@ -47,13 +93,21 @@ void ensemble::generate_ensemble(int rdm, int cdm)
 	       */
 };
 
-void ensemble::compute_minmax(const std::vector<double> &ivector, const char* iname){ 
+
+/*
+void Nextsim::ensemble::generate_ensemble(std::vector<double> rdm, std::vector<double> cdm){
+	cout << "see if compiles" << endl;
+}
+*/
+
+void Nextsim::ensemble::compute_minmax(const std::vector<double> &ivector, const char* iname){ 
 	double max = *max_element(ivector.begin(), ivector.end()); 
 	double min = *min_element(ivector.begin(), ivector.end()); 
 	cout << iname << ":\t max value= " << max << '\t' << "min value= " << min << endl;
 };
 
-void ensemble::compute_vmean(const std::vector<double> &ivector, const char* iname){ 
+void Nextsim::ensemble::compute_vmean(const std::vector<double> &ivector, const char* iname){ 
 	float average = std::accumulate(ivector.begin(), ivector.end(), 0.0)/ivector.size(); 
 	cout << iname << ":\t mean value= " << average << endl;
+};
 };
