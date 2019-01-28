2f3f76c16e18f2b2014471d39a6b3dbf1ab0e062
Ali Aydogdu <ali.aydogdu@nersc.no>
2019-01-28 14:34:07 +0100

    remove unnecessary code and call rand_update in mod_random_forcing

--- modules/enkf/perturbation/src/p_pseudo2D_fld.F90
+++ modules/enkf/perturbation/src/p_pseudo2D_fld.F90
@@ -13,10 +13,9 @@ contains
         implicit none 
 
 
-        character(len=80) outfile
-
-        real rv,rh, alp,bet
         integer i,j,n1,n2
+
+        real current_time
 
         !-- CHeCK: idm, jdm to be read from namelist file --!
         !-- CHeCK: remove the computations in this routine \
@@ -25,45 +24,10 @@ contains
         integer, parameter :: jdm=360
 
         integer k,l,iostat
-        real, allocatable, dimension(:,:) :: fld2d,ranfld,accranfld
-
-        ! Initialize
-        allocate(fld2d    (idm,jdm))
-        allocate(ranfld   (idm,jdm))
-        allocate(accranfld(idm,jdm))
-
-        rv=2.0e00  ! vertical correlation range (in nb of layers)
-        rh=200.00/3.0   ! horizontal decorrelation scale (number of grid cells)
-
-        !  Generates the vertical correlation of the ensemble.
-        alp=exp(-1.0/rv)
-        bet=sqrt(1.0-alp**2) ! keeps var(enstmp)=var(ensmem)
-
-        ! FFT dimensions are powers of 2 for best performance 
-        n1=2**(ceiling(log(float(idm))/log(2.)))
-        n2=2**(ceiling(log(float(jdm))/log(2.)))
-        ! set file names
-        outfile='pseudo2D_fields.dat'
-
 
         call init_fvars
         call init_rand_update
-        call set_random_seed
 
-        open (unit=801,file=trim(outfile),status='replace')
-
-
-        call rand_update
-
-        do i=1,idm 
-        do j=1,jdm 
-           write(801,'(2i6,f6.2)') i,j,ranfld(i,j)
-        end do  
-        end do  
-
-        close(801)
-
-        deallocate(accranfld,fld2d,ranfld)
       end subroutine p_pseudo2D_fld_sub
 
 end module p_pseudo2D_fld
