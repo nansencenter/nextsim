4cc42dc30e41f6bd6f03e91bcbe93d8c957076c2
Ali Aydogdu <ali.aydogdu@nersc.no>
2019-02-04 14:55:22 +0100

    function names changed and indentation

--- modules/enkf/perturbation/include/ensemble.hpp
+++ modules/enkf/perturbation/include/ensemble.hpp
@@ -2,13 +2,13 @@
 Header file for ensemble.cpp
 @author= -aLi- <ali.aydogdu@nersc.no>
 - This class calls fortran objects in order to create synoptic perturbations to generate an ensemble using neXtSIM
-- It reads the output file from ran_update and ports it to related forcing data in neXtSIM 
+- It reads the output file from ran_update and ports it to related forcing data in neXtSIM
 */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
-#include <numeric>                                                              
+#include <numeric>
 #include <vector>
 #include <fstream>
 #include <string>
@@ -18,86 +18,86 @@ extern "C" {
 	double p_pseudo2D_fld_sub();
 /* Other subroutines in libpseudo2D.dylib fortran library
 	void init_fvars_();
-        void m_set_random_seed_(); 
-	void memory_stack_(); 
-	void mod_forcing_nersc_(); 
-	void mod_pseudo_(); 
-	void mod_random_forcing_(); 
-	void assign_force_(); 
-	void assign_vars_(); 
-	void var_sqrt_(); 
+        void m_set_random_seed_();
+	void memory_stack_();
+	void mod_forcing_nersc_();
+	void mod_pseudo_();
+	void mod_random_forcing_();
+	void assign_force_();
+	void assign_vars_();
+	void var_sqrt_();
 */
 }
 
 using namespace std;
 
-namespace Nextsim 
+namespace Nextsim
 {
-class ensemble
-{
-public:
+	class ensemble
+	{
+		public:
 
-	struct synoptic_forcing { 
+			struct synoptic_forcing {
 
-		struct zonal_wind {
-		std::vector<double> data; 
-		const int id  = 2; 
-		static constexpr const char* name  = "uwind"; 
-		} uwind;
+				struct zonal_wind {
+					std::vector<double> data;
+					const int id  = 2;
+					static constexpr const char* name  = "uwind";
+				} uwind;
 
 
-		struct merid_wind {
-		std::vector<double> data; 
-		const int id  = 3; 
-		static constexpr const char* name  = "vwind"; 
-		} vwind;
+				struct merid_wind {
+					std::vector<double> data;
+					const int id  = 3;
+					static constexpr const char* name  = "vwind";
+				} vwind;
 
-		struct temperature {
-		std::vector<double> data; 
-		const int id  = 4; 
-		static constexpr const char* name  = "t2air"; 
-		} t2air;
+				struct temperature {
+					std::vector<double> data;
+					const int id  = 4;
+					static constexpr const char* name  = "t2air";
+				} t2air;
 
-		struct pressure {
-		std::vector<double> data; 
-		const int id  = 5; 
-		static constexpr const char* name  = "slp"; 
-		} slp;
+				struct pressure {
+					std::vector<double> data;
+					const int id  = 5;
+					static constexpr const char* name  = "slp";
+				} slp;
 
-		struct precipitation {
-		std::vector<double> data; 
-		const int id  = 6; 
-		static constexpr const char* name  = "precip"; 
-		} precip;
+				struct precipitation {
+					std::vector<double> data;
+					const int id  = 6;
+					static constexpr const char* name  = "precip";
+				} precip;
 
-		struct humidity {
-		std::vector<double> data; 
-		const int id  = 7; 
-		static constexpr const char* name  = "relhum"; 
-		} relhum;
+				struct humidity {
+					std::vector<double> data;
+					const int id  = 7;
+					static constexpr const char* name  = "relhum";
+				} relhum;
 
-	} synoptic;
+			} synoptic;
 
-private: 
-	
+		private:
 
-	std::vector<std::string> ranfile = { "/docker_io/synforc.00", "/docker_io/synforc.01" };
 
-public:
+			std::vector<std::string> ranfile = { "/docker_io/synforc.00", "/docker_io/synforc.01" };
 
-	void synopticPerturbation();
+		public:
 
-	void synopticPerturbation(int);
+			void synopticPerturbation();
 
-	void addPerturbation();
+			void synopticPerturbation(int);
 
-	void addPerturbation(int);
+			void addPerturbation();
 
-	void addPerturbation(std::vector<double>&, std::vector<double>&, int, int);
-	
-	void computeMinMax(const std::vector<double> &, const char*); 
+			void addPerturbation(int);
 
-	void computeVecMean(const std::vector<double> &, const char*);
+			void addPerturbation(std::vector<double>&, std::vector<double>&, int, int);
 
-};
+			void computeMinMax(const std::vector<double> &, const char*);
+
+			void computeVecMean(const std::vector<double> &, const char*);
+
+	};
 };
