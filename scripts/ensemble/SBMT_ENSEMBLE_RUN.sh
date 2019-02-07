4f981d0fbe37580c1f3055ded7d437eebba4b0aa
alileoalilei <aydogdu.ila@gmail.com>
2019-02-07 15:35:40 +0100

    job submission with envfile

--- scripts/ensemble/SBMT_ENSEMBLE_RUN.sh
+++ scripts/ensemble/SBMT_ENSEMBLE_RUN.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
 . neXtSIM_io_paths.env      # paths to relevant directories
-. neXtSIM_ensemble.env      # experiment-specific environments
 . ensemble_run_function.sh  # functions used in this script
+. ${DYNFILE}
 
 checkpath ${ENSPATH}        # check if ensemble root directory/create
 checkpath ${EnKFDIR}        # check if ensemble filter directory/create
 
 ENSEMBLE+=()                # copy files to ensemble members' paths
 for (( mem=1; mem<=${ESIZE}; mem++ )); do
-	MEMBER=$(leadingzero 2 ${mem})
+    MEMBER=$(leadingzero 2 ${mem})
         MEMNAME=ENS${MEMBER}
-	ENSEMBLE+=([${mem}]=${MEMNAME})
-	MEMPATH=${ENSPATH}/${MEMNAME}; checkpath ${MEMPATH}
-	SRUN=run_${EXPNAME}_${MEMNAME}.sh
+    ENSEMBLE+=([${mem}]=${MEMNAME})
+    MEMPATH=${ENSPATH}/${MEMNAME}; checkpath ${MEMPATH}
+    SRUN=run_${EXPNAME}_${MEMNAME}.sh
 
-	${COPY} ${RUNPATH}/${CONFILE} ${MEMPATH}/${CONFILE}
-	${COPY} ${SCRPATH}/${RUNFILE} ${MEMPATH}/${SRUN}
+    ${COPY} ${RUNFILE} ${MEMPATH}/${SRUN}
+    ${COPY} ${RUNPATH}/${ENVFILE} ${MEMPATH}/.
 
-	sed -e "s;^MEMNAME=.*$;MEMNAME="${MEMNAME}";g" \
-            -e "s;^MEMPATH=.*$;MEMPATH="${MEMPATH}";g" \
-                ${RUNPATH}/${DYNFILE} > ${MEMPATH}/${ENVFILE}
+    sed "s;^exporter_path=.*$;exporter_path="${MEMPATH}";g"\
+        ${RUNPATH}/${CONFILE} > ${MEMPATH}/${CONFILE}
 
-	if [ ${mem} -gt "1" ];then 
-		while kill -0 "$XPID"; do
-		    echo "Process still running... ${XPID}"
-		    sleep 120
-		done
-	fi
+    sed -e "s;^MEMNAME=.*$;MEMNAME="${MEMNAME}";g" \
+        -e "s;^MEMPATH=.*$;MEMPATH="${MEMPATH}";g" \
+        ${RUNPATH}/${DYNFILE} > ${MEMPATH}/${SRCFILE}
+
+    if [ ${mem} -gt "1" ];then
+        while kill -0 "$XPID"; do
+            echo "Process still running... ${XPID}"
+            sleep 120
+        done
+    fi
 # submit nextsim run on docker for the ensemble member #${mem}
-	cd ${MEMPATH}; ID=$(getpid ./${SRUN} ${NPROC} ${LOGFILE}) 
-        XPID=${ID}
+    cd ${MEMPATH}; ID=$( getpid ./${SRUN} ${CONFILE} ${NPROC} ${ENVFILE} )
+    XPID=${ID};echo ${XPID}
 done
