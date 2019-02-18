51582cdc9eca33452b7c2735d6076ca4d04be0e5
Ali Aydogdu <ali.aydogdu@nersc.no>
2019-02-18 14:16:31 +0100

    for standalone perturbations only; paths updated

--- modules/enkf/perturbation/script/run_pseudo2D.sh
+++ modules/enkf/perturbation/script/run_pseudo2D.sh
@@ -4,9 +4,11 @@ BOOST_DIR=/opt/local/boost
 BOOST_LIBDIR=${BOOST_DIR}/lib
 
 WD=/y1/aydogdu/GIT/nextsim/modules/enkf/perturbation
-BIN=./bin/p_pseudo2D 
-LIB=./lib/libpseudo2D.dylib
-SRC=${WD}/src
+IOP=../IO
+BIN=../bin/p_pseudo2D
+LIB=../lib/libpseudo2D.dylib
+SRC=../src
+NML=../nml/pseudo2D.nml
 
 MAKEFILE='make -f makefile.standalone'
 
@@ -14,17 +16,19 @@ export DYLD_LIBRARY_PATH=${BOOST_LIBDIR}:${WD}/lib
 
 echo "number of args: $#"
 
+cd ${WD}/script
+cp ${NML} ${IOP}/.
 # Compiles the whole code if there are more than 2 arguments
 [[ $# -ge 3 ]] &&\
-       	cd ${SRC} && ${MAKEFILE} clean && ${MAKEFILE} all && cd ../
+       cd ${SRC} && ${MAKEFILE} clean && ${MAKEFILE} all && cd ../
 
 # Compiles the if fortran library is missing
 [[ !  -f ${LIB} ]] &&\
-       	cd ${SRC} && ${MAKEFILE} clean && ${MAKEFILE} lib && ${MAKEFILE} bin && cd ../
+       cd ${SRC} && ${MAKEFILE} clean && ${MAKEFILE} lib && ${MAKEFILE} bin && cd ../
 
 # Compiles the binary if the executable is missing
 [[ !  -f ${BIN} ]] &&\
-       	cd ${SRC} && ${MAKEFILE} bin && cd ../
+       cd ${SRC} && ${MAKEFILE} bin && cd ../
 
 [[ $# -eq 0 ]] && \
 	echo "Usage: args -> run and/or make and/or plot " &&\
@@ -34,10 +38,11 @@ echo "number of args: $#"
 
 # Runs if there is only one argument
 [[ $# -ge 1 ]] &&\
-       	cd ${WD} && ${BIN}
+       cd ${WD}/bin && ${BIN}
 
 # Plots if one of the arguments is 'plot'
 [[ $# -ge 2 ]] && ( [[ $1 = 'plot' ]] || [[ $2 = 'plot' ]] ) &&\
+       cd ${WD}
        source activate FERRET &&\
        pyferret -png -script POST/frt.pseudo.jnl &&\
        cd data/ASR  && \
