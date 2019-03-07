0634e5e1ddb7ad0c837fbb87b26027cc21ab50b2
Timothy Williams <tdcwilliams@gmail.com>
2019-03-07 12:59:16 +0100

    Update model/finiteelement.cpp
    Co-Authored-By: einola <einar.olason@nersc.no>

--- model/finiteelement.cpp
+++ model/finiteelement.cpp
@@ -6131,7 +6131,7 @@ FiniteElement::init()
     // The constant factor converts between M_res_root_mesh and the node spacing (it is approximate)
     t_damage = M_res_root_mesh*1.3429*std::pow(young/(2.0*(1.0+nu0)*physical::rhoi),-0.5);  //Characteristic time for the propagation of damage
     if (dtime_step/t_damage > 10.0)
-        LOG(WARNING) << "For best deformation scaling results, the ratio Deltat/td should be < 10. THE SPIRIT OF VERO IS WATCHING YOU\n";
+        LOG(WARNING) << "For best deformation scaling results, the ratio simul.timestep/t_damage should be < 10. (Currently it is " << dtime_step/t_damage << ").  THE SPIRIT OF VERO IS WATCHING YOU\n";
 
     if ( M_use_restart )
     {
