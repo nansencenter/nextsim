951737e1cbfa8ebb61959fb726b2ffa6bc84b003
Einar Örn Ólason <einar.olason@nersc.no>
2019-05-13 12:31:35 +0200

    Shorten long lines
    Co-Authored-By: Timothy Williams <tdcwilliams@gmail.com>

--- model/finiteelement.cpp
+++ model/finiteelement.cpp
@@ -4628,7 +4628,8 @@ FiniteElement::update()
             {
                 double sigma_dot_i = 0.0;
                 for(int j=0;j<3;j++)
-                    sigma_dot_i += std::exp(damaging_exponent*(1.-M_conc[cpt]))*young*(1.-old_damage)*M_Dunit[3*i + j]*epsilon_veloc[j];
+                     sigma_dot_i += std::exp(damaging_exponent*(1.-M_conc[cpt]))
+                         *young*(1.-old_damage)*M_Dunit[3*i + j]*epsilon_veloc[j];
 
                 sigma[i] = (M_sigma[i][cpt]+time_step*sigma_dot_i)*multiplicator;
             }
