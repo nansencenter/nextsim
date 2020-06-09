a44fcc83a03e2535b1ae7f495ef3b3cd2bfb8c12
Sukun Cheng <sukun.cheng@nersc.no>
2020-06-09 09:01:33 +0800

    allocate 3d variables to read data correctly

--- modules/enkf/enkf-c/prep/reader_smos.c
+++ modules/enkf/enkf-c/prep/reader_smos.c
@@ -275,11 +275,12 @@ void reader_smos_standard3(char* fname, int fid, obsmeta* meta, grid* g, observa
     int varid_lon, varid_lat, varid_sit, varid_error, varid_time;
     int sit_fill_value;
     int estd_fill_value;
-    float* lon = NULL;
-    float* lat = NULL;
-    float* sit = NULL;
-    float* error_std = NULL;
-    float* time = NULL;
+    float sit_scale_factor,estd_scale_factor;
+    float** lon;
+    float** lat;
+    int*** sit;
+    int*** error_std;
+    double* time = NULL;
     int year, month, day;
     char tunits[MAXSTRLEN];
     size_t tunits_len;
@@ -303,32 +304,36 @@ void reader_smos_standard3(char* fname, int fid, obsmeta* meta, grid* g, observa
     enkf_printf("        x = %u\n y = %u\n t = %u\n nobs = %u\n", xlen, ylen, tlen, nobslen);
 
     ncw_inq_varid(ncid, "lat", &varid_lat);
-    lat = malloc(ylen*xlen*sizeof(float));
-    ncw_get_var_float(ncid, varid_lat, lat);
+    lat = alloc2d(ylen, xlen, sizeof(float));  // refer to reader_amsre.c,  using 2d array is easier for understanding. sit[i*j+j]] in standard2 is a bug to be fixed in future
+    ncw_get_var_float(ncid, varid_lat, lat[0]);
 
     ncw_inq_varid(ncid, "lon", &varid_lon);
-    lon = malloc(ylen*xlen*sizeof(float));
-    ncw_get_var_float(ncid, varid_lon, lon);
+    lon = alloc2d(ylen, xlen, sizeof(float));
+    ncw_get_var_float(ncid, varid_lon, lon[0]);
 
     ncw_inq_varid(ncid, "analysis_sea_ice_thickness", &varid_sit);
-    sit = malloc(tlen*ylen*xlen*sizeof(float));
-    ncw_get_var_float(ncid, varid_sit, sit);
+    sit = alloc3d(tlen, ylen, xlen, sizeof(int));
+    ncw_get_var_int(ncid, varid_sit, sit[0][0]);
     if (ncw_att_exists(ncid, varid_sit, "_FillValue"))
         ncw_get_att_int(ncid, varid_sit, "_FillValue", &sit_fill_value);
     else if (ncw_att_exists(ncid, varid_sit, "missing_value"))
         ncw_get_att_int(ncid, varid_sit, "missing_value", &sit_fill_value);
+    if (ncw_att_exists(ncid, varid_sit, "scale_factor"))
+        ncw_get_att_float(ncid, varid_sit, "scale_factor", &sit_scale_factor);
 
     ncw_inq_varid(ncid, "analysis_sea_ice_thickness_unc", &varid_error);
-    error_std = malloc(tlen*ylen*xlen*sizeof(float));
-    ncw_get_var_float(ncid, varid_error, error_std);
+    error_std = alloc3d(tlen, ylen, xlen, sizeof(int));
+    ncw_get_var_int(ncid, varid_error, error_std[0][0]);
     if (ncw_att_exists(ncid, varid_error, "_FillValue"))
         ncw_get_att_int(ncid, varid_error, "_FillValue", &estd_fill_value);
     else if (ncw_att_exists(ncid, varid_error, "missing_value"))
         ncw_get_att_int(ncid, varid_error, "missing_value", &estd_fill_value);
+    if (ncw_att_exists(ncid, varid_error, "scale_factor"))
+        ncw_get_att_float(ncid, varid_error, "scale_factor", &estd_scale_factor);
 
     ncw_inq_varid(ncid, "time", &varid_time);
-    time = malloc(tlen * sizeof(float));
-    ncw_get_var_float(ncid, varid_time, time);
+    time = malloc(tlen * sizeof(double));
+    ncw_get_var_double(ncid, varid_time, time);
     ncw_inq_attlen(ncid, varid_time, "units", &tunits_len);
     ncw_get_att_text(ncid, varid_time, "units", tunits);
     basename[37] = 0;
@@ -346,38 +351,39 @@ void reader_smos_standard3(char* fname, int fid, obsmeta* meta, grid* g, observa
 
     tunits_convert(tunits, &tunits_multiple, &tunits_offset);
 
-    for (int i = 0; i < (int) ylen; ++i) {
-        for (int j = 0; j < (int) xlen; ++j) {
+    for (int it= 0; it< (int) tlen; ++it) {
+        for (int i = 0; i < (int) ylen; ++i) {
+            for (int j = 0; j < (int) xlen; ++j) {
 
-            observation* o;
+                observation* o;
 
-            obs_checkalloc(obs);
-            o = &obs->data[obs->nobs];
+                obs_checkalloc(obs);
+                o = &obs->data[obs->nobs];
 
-            o->product = st_findindexbystring(obs->products, meta->product);
-            assert(o->product >= 0);
-            o->type = obstype_getid(obs->nobstypes, obs->obstypes, meta->type, 1);
-            o->instrument = st_add_ifabsent(obs->instruments, "SMOS", -1);
-            o->id = obs->nobs;
-            o->fid = fid;
-            o->batch = 0;
-            o->value = sit[i*j+j];
-//            o->std = error_std[i*j+j]; # there are zeros in the smos data for std. Not working for EnKF
-            o->std = .02;
-            o->lon = lon[i*j+j];
-            o->lat = lat[i*j+j];
-            o->depth = 0.0;
-            o->fk = (double) ksurf;
-            o->status = grid_xy2fij(g, o->lon, o->lat, &o->fi, &o->fj);
-//            enkf_printf("   lon = %f lat = %f\n val= %f\n std = %f\n", o->lon, o->lat, o->value, o->std);
-//            enkf_printf("   sit = %f\n", o->value);
-            if (!obs->allobs && o->status == STATUS_OUTSIDEGRID)
-               continue;
-            o->model_depth = NAN;   // set in obs_add()
-            o->date = time[tlen] * tunits_multiple + tunits_offset;
-            o->aux = -1;
+                o->product = st_findindexbystring(obs->products, meta->product);
+                assert(o->product >= 0);
+                o->type = obstype_getid(obs->nobstypes, obs->obstypes, meta->type, 1);
+                o->instrument = st_add_ifabsent(obs->instruments, "SMOS", -1);
+                o->id = obs->nobs;
+                o->fid = fid;
+                o->batch = 0;
+                o->value = (double) (sit[it][i][j]*sit_scale_factor);
+                o->std = (double) (error_std[it][i][j]*estd_scale_factor);
+                o->lon = lon[i][j];
+                o->lat = lat[i][j];
+                o->depth = 0.0;
+                o->fk = (double) ksurf;
+                o->status = grid_xy2fij(g, o->lon, o->lat, &o->fi, &o->fj);
+    //            enkf_printf("   lon = %f lat = %f\n val= %f\n std = %f\n", o->lon, o->lat, o->value, o->std);
+    //            enkf_printf("   sit = %f\n", o->value);
+                if (!obs->allobs && o->status == STATUS_OUTSIDEGRID)
+                    continue;
+                o->model_depth = NAN;   // set in obs_add()
+                o->date = time[tlen] * tunits_multiple + tunits_offset;
+                o->aux = -1;
 
-            obs->nobs++;
+                obs->nobs++;
+            }
         }
     }
 
