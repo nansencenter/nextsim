9135549253a167a88b7624ee8c4059459733c890
Einar Olason <einar.olason@nersc.no>
2020-08-21 08:59:49 +0200

    Increase the snow and ice thickness limits and clean up a bit. Seems to be fine now.

--- model/finiteelement.cpp
+++ model/finiteelement.cpp
@@ -14182,8 +14182,8 @@ FiniteElement::checkFieldsFast()
     // common sense min/max
     boost::unordered_map<std::string, std::pair<double,double>>
         minmax = boost::assign::map_list_of
-            ("M_thick",      std::make_pair(   0., 35.))
-            ("M_snow_thick", std::make_pair(   0.,  5.))
+            ("M_thick",      std::make_pair(   0., 50.))
+            ("M_snow_thick", std::make_pair(   0., 10.))
             ("M_conc",       std::make_pair(   0.,  1.))
             ("M_damage",     std::make_pair(   0.,  1.))
             ("M_tice",       std::make_pair(-100.,  0.))
@@ -14203,6 +14203,7 @@ FiniteElement::checkFieldsFast()
 
     bool crash = false;
     std::stringstream crash_msg;
+    crash_msg << "FiniteElement::checkFieldsFast: Check failed: ";
 
     // Loop over all the nodal variables
     for (auto ptr: M_variables_elt)
@@ -14257,7 +14258,7 @@ FiniteElement::checkFieldsFast()
         M_comm.barrier();
         throw std::runtime_error(crash_msg.str());
     }
-}
+} //checkFieldsFast
 
 // -------------------------------------------------------------------------------------
 //! Checks fields for NaNs and for too big ice thickness values.
