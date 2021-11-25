ed900a9df8f360304720db439d2bb2ac08d89630
sukuncheng <sukun.cheng@nersc.no>
2021-11-25 16:29:50 +0100

    recover AssimConC, AssimThick 3ec63430

--- model/externaldata.cpp
+++ model/externaldata.cpp
@@ -313,7 +313,7 @@ void ExternalData::check_and_reload(std::vector<double> const& RX_in,
                             int j = M_dataset->grid.reduced_nodes_ind[n]/x_count;
                             int i = M_dataset->grid.reduced_nodes_ind[n] - j*x_count;
                             i = x_start + (y_start + j)*N_full + i;
-                            M_dataset->variables[0].loaded_data[it][n] += synforc1[i]*( 1- M_dataset->variables[3].loaded_data[it][n]);
+                            M_dataset->variables[0].loaded_data[it][n] += synforc1[i]*( 1- M_dataset->variables[3].loaded_data[it][n]); //limit sst perturbation to ice covered area, variables[3] is conc, variables[0] is sst
                             M_dataset->variables[1].loaded_data[it][n] += synforc2[i]; 
                             if (i >= MN_full) {LOG(DEBUG)<<"maxmium synforc exceeded\n";}
                         }
