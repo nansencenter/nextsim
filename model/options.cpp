70dba0ef9568f233f9d87fe60b0764f586c59ad3
tdcwilliams <tdcwilliams@gmail.com>
2025-10-23 10:18:26 +0200

    add help for moorings.parallel_output

--- model/options.cpp
+++ model/options.cpp
@@ -157,7 +157,7 @@ namespace Nextsim
             ("moorings.grid_transpose", po::value<bool>()->default_value( false ), "If true we assume the first dimension is x and the second y (non-standard ordering).")
             ("moorings.false_easting", po::value<bool>()->default_value( true ),
                 "true: we output vectors relative to the output grid; false: we give their north-south components. NB only implemented for grid_type=regular")
-            ("moorings.parallel_output", po::value<bool>()->default_value( false ), "")
+            ("moorings.parallel_output", po::value<bool>()->default_value( false ), "Use true to save netcdf files in parallel. Note that this is only faster for larger meshes.")
 
 
             // -- drifters
